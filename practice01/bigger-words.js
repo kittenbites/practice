9a180994e7a846ac9006c4a063b615440d847c64
kittenbites <sammie.nagle@gmail.com>
2019-12-14 09:55:48 -0800

    Practice 1 *with filter

--- practice01/bigger-words.js
+++ practice01/bigger-words.js
@@ -1,3 +1,8 @@
 function biggerWords(str,arr){
   return arr.filter((val)=>val.length>str.length)
 }
+
+var myArray = ['bug', 'insect', 'mammal', 'reptile', 'mammoth', 'turtles']
+
+console.log(`Practice 1:
+biggerWords output: ${biggerWords('whales', myArray)}`)

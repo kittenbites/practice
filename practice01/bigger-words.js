290d6388625106d7d32359c2a62d119fdc20a798
kittenbites <sammie.nagle@gmail.com>
2019-12-14 10:06:03 -0800

    Practice 1 *without filter

--- practice01/bigger-words.js
+++ practice01/bigger-words.js
@@ -2,7 +2,18 @@ function biggerWords(str,arr){
   return arr.filter((val)=>val.length>str.length)
 }
 
+function biggerWords2(str,arr){
+  let returnArr = []
+  for(let word of arr){
+    if (word.length>str.length){
+      returnArr.push(returnArr);
+    }
+  }
+  return returnArr;
+}
+
 var myArray = ['bug', 'insect', 'mammal', 'reptile', 'mammoth', 'turtles']
 
 console.log(`Practice 1:
-biggerWords output: ${biggerWords('whales', myArray)}`)
+biggerWords output: ${biggerWords('whales', myArray)}
+biggerWords2 output: ${biggerWords2('whales', myArray)}`)

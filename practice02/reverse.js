d91be1cab6d9a4ec0655d1fe2f8b515751f322a7
kittenbites <sammie.nagle@gmail.com>
2019-12-14 10:42:11 -0800

    Practice 2 *reverse in place

--- practice02/reverse.js
+++ practice02/reverse.js
@@ -7,7 +7,19 @@ function arrayReverse(arr){
   return returnArr;
 }
 
-var myArray = ['hello', 45, 'Bob', 'what', '23']
+function reverseInPlace(arr) {
+  for(let index = 0;index<Math.floor(arr.length/2);index++){
+    const temp = arr[index];
+    arr[index] = arr[arr.length-index-1];
+    arr[arr.length - index - 1] = temp;
+  }
+  return arr;
+}
+
+var myArray = ['hello', 45, 'Bob', 'what', '23',45]
 
 console.log(`Practice 2:
   arrayReverse output: ${ arrayReverse(myArray) }`);
+
+console.log(`Practice 2:
+  reverseInPlace output: ${ reverseInPlace(myArray)}`);

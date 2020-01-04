fa195d39135bb12a253d9fdc150d76ce405a8bf0
kittenbites <sammie.nagle@gmail.com>
2020-01-03 23:20:31 -0800

    Finished solution for group array practice problem

--- practice03/group-array.js
+++ practice03/group-array.js
@@ -1,2 +1,16 @@
-
+function groupArray(arr){
+  let returnArr = [];
+  const typeMap = {};
+  for (let item of arr){
+    if (!typeMap[typeof (item)]){
+      typeMap[typeof(item)] = [];
+    }
+    typeMap[typeof (item)].push(item);
+  }
+  for(let typeArr in typeMap){
+    returnArr.push(typeMap[typeArr]);
+  }
+  return returnArr;
+}
 var myArray = ['hello', 34, true, false, 'goodbye', 56, 12, '25', true];
+console.log("groupArray result:", groupArray(myArray));
